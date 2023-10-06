8fd4da3a0cd5ad16246bf46f27f41298002d87bc
nesladka <147166345+nesladka@users.noreply.github.com>
2023-10-06 17:23:04 +0300

    Update main.py

--- pz1-2/main.py
+++ pz1-2/main.py
@@ -15,6 +15,9 @@ plt.plot(x1, (141 - 4 * x1) / 5, label='4*X1 + 5*X2 = 141', color='blue', linest
 plt.axvline(x=19, color='purple', linestyle='dotted', label='x1=19')  
 plt.axhline(y=17, color='orange', linestyle='dotted', label='x2=17') 
 
-plt.arrow(0, 0, 3, 5, head_width=0.5, head_length=0.7, fc='red', ec='red', label='вектор z (3, 5)')  # Changed color
+plt.arrow(0, 0, 3, 5, head_width=0.5, head_length=0.7, fc='red', ec='red', label='вектор z (3, 5)') 
+
+perpendicular_x = np.array([5, -5])
+perpendicular_y = np.array([-3, 3])
 
 
